2d02c25049374718e5af810311cde18839aa9d2a
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2024-11-30 21:19:15 +0900

    Add files via upload

---
+++ ESP32_GetIPAddress.ino
@@ -0,0 +1,24 @@
+#include <WiFi.h>
+const char* ssid = "Ria2G";       
+const char* password = "730124go";
+
+void setup() {
+    Serial.begin(115200);
+    Serial.print("\nConnecting to " + String(ssid));
+    //Serial.println(ssid);
+
+    // 해당 공유기에 접속 시도
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println(" WiFi Connected");
+
+    // ESP32 보드가 공유기로부터 할당 받은 "(사설)IP 주소"
+    Serial.print("Local IP address : " + WiFi.localIP().toString());
+}
+
+void loop() {
+}
+

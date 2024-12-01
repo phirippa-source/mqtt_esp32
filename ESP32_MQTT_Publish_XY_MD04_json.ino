21afb38e1871ae305d5bd94176f963d4752daefd
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2024-12-01 15:20:43 +0900

    Add files via upload

---
+++ ESP32_MQTT_Publish_XY_MD04_json.ino
@@ -0,0 +1,71 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// XY-MD04 ---------------------------------------
+#define RS485RW     4
+#define WRITE_MODE  HIGH
+#define READ_MODE   LOW
+
+byte cmd[] = {0x01, 0x04, 0x00, 0x01, 0x00, 0x02, 0x20, 0x0b};
+HardwareSerial RS485(2); // Serial2를 RS485로 지정
+// ----------------------------------------------
+
+const char* ssid = "Ria2G";
+const char* password = "730124go";
+const char* serverIPAddr = "192.168.0.16";
+const char* userId = "mqtt_ship";
+const char* userPw = "1234";
+const char* clientId = "unique client ID";
+const char* topic = "MyOffice/Outdoor/Sensor";
+
+
+WiFiClient wifiClient; 
+PubSubClient client(serverIPAddr, 1883, wifiClient);
+
+void setup() {
+    Serial.begin(115200);
+    Serial.print("\nConnecting to " + String(ssid));
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println(" WiFi Connected");   
+
+    Serial.printf("\nConnecting to the Broker(%s)\n", serverIPAddr);
+    while ( !client.connect(clientId, userId, userPw) ){ 
+        Serial.print("*");
+        delay(500);
+    }
+    Serial.println("Connected to the Broker!!!");  
+    // XY-MD04
+    RS485.begin(9600, SERIAL_8N1, 16, 17);
+    pinMode(RS485RW, OUTPUT);  
+}
+
+const int PAYLOAD_SIZE = 128;
+void loop() {
+    float t, h;
+    byte rxBuf[9];
+    char buf[8], payload[PAYLOAD_SIZE];
+
+    digitalWrite(RS485RW, WRITE_MODE);
+    RS485.write(cmd, sizeof(cmd));
+    RS485.flush();
+    digitalWrite(RS485RW, READ_MODE);
+
+    if(RS485.readBytes(rxBuf, 9)) {
+        t = ((rxBuf[3] << 8) | rxBuf[4]) / 10.0;
+        h = ((rxBuf[5] << 8) | rxBuf[6]) / 10.0;
+    
+        String sPayload = "{\"Temp\":"
+                            + String(t, 1)
+                            + ",\"Humi\":"
+                            + String(h, 1)
+                            + "}";
+        sPayload.toCharArray(payload, PAYLOAD_SIZE);
+        client.publish(topic, payload);
+        Serial.print(String(topic) + " "); Serial.println(payload); 
+    }
+    delay(2000);
+}
